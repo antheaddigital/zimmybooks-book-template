048c52b8502912bdc5f4c9ebd20bda93f6bb0f41
camachowebworld <camachowebworld@yahoo.com>
2016-07-15 12:24:48 -0700

    changed to page reload on windows change

--- js/main.js
+++ js/main.js
@@ -1,35 +1,32 @@
 $(document).ready(function(){
 
-  // Slider functionality
-  function slickReinit(){
-    $('.slider').on('init', function(event, slick){
-      // Apply browser height to wrapper and slider blocks
-      var browserHeight = $(window).height();
-      $('.wrapper').height(browserHeight);
-      console.log(browserHeight);
-      var sliderMargin = Math.ceil(browserHeight / 20);
-      $('.slider .page-img').height(browserHeight - (sliderMargin * 2));
-      $('.slider').css({'margin-top': sliderMargin});
-      // Apply new image width to slider width
-      var pageImgWidth = $('.slider .page-img').width();
-      $('.slider').width(pageImgWidth);
-      // Apply width of sign link and position
-      var signLinkWidth = Math.ceil(pageImgWidth / 5);
-      $('.sign-link').width(signLinkWidth);
-      var signLinkPosition = Math.ceil(signLinkWidth / 10);
-      $('.sign-link').css({ top: signLinkPosition, right: signLinkPosition});
-    });
-    $('.slider').slick({
-      infinite: true,
-      dots: false,
-      fade: true,
-      cssEase: 'linear'
-    });
-  }
-  $(window).on('resize', function(){
-    slickReinit();
+  $('.slider').on('init', function(event, slick){
+    // Apply browser height to wrapper and slider blocks
+    var browserHeight = $(window).height();
+    $('.wrapper').height(browserHeight);
+    console.log(browserHeight);
+    var sliderMargin = Math.ceil(browserHeight / 20);
+    $('.slider .page-img').height(browserHeight - (sliderMargin * 2));
+    $('.slider').css({'margin-top': sliderMargin});
+    // Apply new image width to slider width
+    var pageImgWidth = $('.slider .page-img').width();
+    $('.slider').width(pageImgWidth);
+    // Apply width of sign link and position
+    var signLinkWidth = Math.ceil(pageImgWidth / 5);
+    $('.sign-link').width(signLinkWidth);
+    var signLinkPosition = Math.ceil(signLinkWidth / 10);
+    $('.sign-link').css({ top: signLinkPosition, right: signLinkPosition});
   });
-  slickReinit();
+  $('.slider').slick({
+    infinite: true,
+    dots: false,
+    fade: true,
+    cssEase: 'linear'
+  });
+
+  $(window).on('resize', function(){
+    location.reload();
+  });
 
   // Sign pop-up functionality
   $('.sign-link').magnificPopup({type:'image'});
